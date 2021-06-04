48e7a06426c502d6b34a8f5b1cbdee4d9d0e195c
unknown <tamarasantosc@gmail.com>
2021-06-03 21:19:09 -0300

    criei função criptografar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,51 @@
-const cipher = {
-  // ...
-};
+//const cipher = {
 
-export default cipher;
+//};
+
+//export default cipher;
+
+// (codigoDaLetra + deslocamento) % tamanhoDoAlfabeto
+// ex: (codigoDaLetra + 7) % 26
+
+
+
+
+
+
+
+
+
+
+
+
+function criptografar() {
+  
+  const mensagem = document.getElementById("denuncia").value;
+  const offset = Number(document.getElementById("seleciona-chave").value);
+  let saida = ""
+  let codigoASCDaLetra;
+  let novoCodigo;
+  let letraCriptografada;
+  let letraAtual;  
+ 
+  for (var i = 0; i < mensagem.length; i++) {    
+    letraAtual = mensagem[i]
+    if(letraAtual !== " ") {
+      codigoASCDaLetra = letraAtual.charCodeAt()
+      novoCodigo = codigoASCDaLetra + offset
+      letraCriptografada = String.fromCharCode(novoCodigo)
+      saida += letraCriptografada
+    } else {
+      saida += " "     
+    }
+   
+  }
+  document.getElementById("mensagem-criptografada").innerHTML = saida
+ 
+}
+
+// console.log(((codigoASCDaLetra - 65 + offset) % 26) + 65);
+
+
+
+
