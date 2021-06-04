e2b26e8f4dc3e05366350f403d1456f17c592202
unknown <tamarasantosc@gmail.com>
2021-06-03 22:36:41 -0300

    Criada função descriptografar

--- src/cipher.js
+++ src/cipher.js
@@ -19,33 +19,58 @@
 
 
 function criptografar() {
-  
+
   const mensagem = document.getElementById("denuncia").value;
   const offset = Number(document.getElementById("seleciona-chave").value);
   let saida = ""
   let codigoASCDaLetra;
   let novoCodigo;
   let letraCriptografada;
-  let letraAtual;  
- 
-  for (var i = 0; i < mensagem.length; i++) {    
+  let letraAtual;
+
+  for (var i = 0; i < mensagem.length; i++) {
     letraAtual = mensagem[i]
-    if(letraAtual !== " ") {
+    if (letraAtual !== " ") {
       codigoASCDaLetra = letraAtual.charCodeAt()
       novoCodigo = codigoASCDaLetra + offset
       letraCriptografada = String.fromCharCode(novoCodigo)
       saida += letraCriptografada
     } else {
-      saida += " "     
+      saida += " "
     }
-   
+
   }
   document.getElementById("mensagem-criptografada").innerHTML = saida
- 
+
 }
 
 // console.log(((codigoASCDaLetra - 65 + offset) % 26) + 65);
 
 
+function descriptografar() {
+    
+  const mensagem = document.getElementById("mensagem-criptografada").value;
+  const offset = Number(document.getElementById("seleciona-chave").value);
+  let saida = ""
+  let codigoASCDaLetra;
+  let novoCodigo;
+  let letraCriptografada;
+  let letraAtual;
+
+  for (var i = 0; i < mensagem.length; i++) {
+    letraAtual = mensagem[i]
+    if (letraAtual !== " ") {
+      codigoASCDaLetra = letraAtual.charCodeAt()
+      novoCodigo = codigoASCDaLetra - offset
+      letraCriptografada = String.fromCharCode(novoCodigo)
+      saida += letraCriptografada
+    } else {
+      saida += " "
+    }
+
+
+  }
+  document.getElementById("mensagem-descriptografada").innerHTML = saida
+}
 
 
