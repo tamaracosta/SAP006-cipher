8946cba3b8edcdda178306516153170604e28fdc
unknown <tamarasantosc@gmail.com>
2021-06-10 09:04:35 -0300

    Footer e Lógica offset negativo

--- src/cipher.js
+++ src/cipher.js
@@ -1,76 +1,103 @@
-//const cipher = {
+const cipher = {
+  encode: criptografar,
+  decode: descriptografar
+};
 
-//};
-
-//export default cipher;
-
-// (codigoDaLetra + deslocamento) % tamanhoDoAlfabeto
-// ex: (codigoDaLetra + 7) % 26
-
-
-
-
-
-
-
-
-
-
-
-
-function criptografar() {
-
-  const mensagem = document.getElementById("denuncia").value;
-  const offset = Number(document.getElementById("seleciona-chave").value);
-  let saida = ""
+function criptografar(offset, mensagem) {
+  let letraAtual;
   let codigoASCDaLetra;
   let novoCodigo;
   let letraCriptografada;
-  let letraAtual;
+  let saida = ""
 
-  for (var i = 0; i < mensagem.length; i++) {
+  if (typeof offset !== "number") {
+    throw TypeError('Offset precisa ser númerico')
+  }
+
+  if (typeof mensagem !== "string") {
+    throw TypeError('A Mensagem precisa ser texto')
+  }
+
+  if (offset < 0) {
+    offset = 26 + offset;
+  }
+
+  for (let i = 0; i < mensagem.length; i++) {
     letraAtual = mensagem[i]
-    if (letraAtual !== " ") {
-      codigoASCDaLetra = letraAtual.charCodeAt()
-      novoCodigo = codigoASCDaLetra + offset
+    codigoASCDaLetra = letraAtual.charCodeAt()
+
+    //CODIFICAR OFFSET NEGATIVO LETRA MAIUSCULA
+    if (offset < 0 && codigoASCDaLetra >= 65 && codigoASCDaLetra <= 90) {
+      novoCodigo = (((codigoASCDaLetra - 90 + 26 + (offset)) % 26) + 90)
       letraCriptografada = String.fromCharCode(novoCodigo)
       saida += letraCriptografada
+
+    //CODIFICAR OFFSET POSITIVO LETRA MAIUSCULA
+    } else if (offset > 0 && codigoASCDaLetra >= 65 && codigoASCDaLetra <= 90) {
+      novoCodigo = (((codigoASCDaLetra - 65 + offset) % 26) + 65)
+      letraCriptografada = String.fromCharCode(novoCodigo)
+      saida += letraCriptografada
+
+    // CODIFICAR OFFSET NEGATIVO LETRA MINUSCULA
+    } else if (offset < 0 && codigoASCDaLetra >= 97 && codigoASCDaLetra <= 122) {
+      novoCodigo = (((codigoASCDaLetra - 122 + 26 + (offset)) % 26) + 122)
+      letraCriptografada = String.fromCharCode(novoCodigo)
+      saida += letraCriptografada
+
+    // CODIFICAR OFFSET POSITIVO LETRA MINUSCULA
+    } else if (offset > 0 && codigoASCDaLetra >= 97 && codigoASCDaLetra <= 122) {
+      novoCodigo = (((codigoASCDaLetra - 97 + offset) % 26) + 97)
+      letraCriptografada = String.fromCharCode(novoCodigo)
+      saida += letraCriptografada
+
     } else {
-      saida += " "
+      saida += letraAtual;
     }
 
   }
-  document.getElementById("mensagem-criptografada").innerHTML = saida
-
+  return saida;
 }
 
-// console.log(((codigoASCDaLetra - 65 + offset) % 26) + 65);
 
-
-function descriptografar() {
-    
-  const mensagem = document.getElementById("mensagem-criptografada").value;
-  const offset = Number(document.getElementById("seleciona-chave").value);
-  let saida = ""
+function descriptografar(offset, mensagem) {
+  let letraAtual;
   let codigoASCDaLetra;
   let novoCodigo;
   let letraCriptografada;
-  let letraAtual;
+  let saida = ""
 
-  for (var i = 0; i < mensagem.length; i++) {
+  if (typeof offset !== "number") {
+    throw TypeError('Offset precisa ser númerico')
+  }
+
+  if (typeof mensagem !== "string") {
+    throw TypeError('A Mensagem precisa ser texto')
+  }
+
+  if (offset < 0) {
+    offset = 26 + offset;
+  }
+
+  for (let i = 0; i < mensagem.length; i++) {
     letraAtual = mensagem[i]
-    if (letraAtual !== " ") {
-      codigoASCDaLetra = letraAtual.charCodeAt()
-      novoCodigo = codigoASCDaLetra - offset
+    codigoASCDaLetra = letraAtual.charCodeAt()
+
+    if (codigoASCDaLetra >= 65 && codigoASCDaLetra <= 90) {
+      novoCodigo = (((codigoASCDaLetra + 65 - offset) % 26) + 65)
       letraCriptografada = String.fromCharCode(novoCodigo)
       saida += letraCriptografada
+
+    } else if (codigoASCDaLetra >= 97 && codigoASCDaLetra <= 122) {
+      novoCodigo = 122 - ((122 - codigoASCDaLetra + offset) % 26);
+      letraCriptografada = String.fromCharCode(novoCodigo)
+      saida += letraCriptografada
+
     } else {
-      saida += " "
+      saida += letraAtual;
     }
 
-
   }
-  document.getElementById("mensagem-descriptografada").innerHTML = saida
+  return saida;
 }
 
-
+export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+function criptografar() {
+    const mensagem = document.getElementById("denuncia").value;
+    const offset = Number(document.getElementById("seleciona-chave").value);
+    const saida = cipher.encode(offset, mensagem);
+
+    document.getElementById("mensagem-criptografada").innerHTML = saida
+}
+
+document.getElementById("botao-criptografar").addEventListener("click", criptografar);
+
+
+function descriptografar() {
+    const mensagem = document.getElementById("mensagem-criptografada").value;
+    const offset = Number(document.getElementById("seleciona-chave").value);
+    const saida = cipher.decode(offset, mensagem);
+
+
+    document.getElementById("mensagem-descriptografada").innerHTML = saida
+}
+
+document.getElementById("botao-descriptografar").addEventListener("click", descriptografar);
+
+
